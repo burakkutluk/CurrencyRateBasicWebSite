b5189396a6edc070cfda4833026618db5caf1770
Burak KUTLUK <117195323+burakkutluk@users.noreply.github.com>
2023-04-26 15:25:41 +0300

    Add files via upload

---
+++ Currency/currency.js
@@ -0,0 +1,14 @@
+
+class Currency{
+    constructor(){
+        this.url = "https://api.freecurrencyapi.com/v1/latest?apikey=HCzDUD3kRRAUOXYqcN1q3OEOcALek1Ox6PhQ5KpF&base_currency=";
+    }
+
+  async  exchange(amount , firstCurrency , secondCurrency){
+    const response =    await fetch(`${this.url}${firstCurrency}`);
+    const result = await response.json();
+    const exchangedResult = amount * result.data[secondCurrency];
+
+    return exchangedResult;
+    }
+}
